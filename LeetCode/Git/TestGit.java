34b240c942add9bb6954d44b7a304cbff9517a05
tangbd <tangbd@apsatcom.com>
2021-12-22 11:54:36 +0800

    测试git

---
+++ LeetCode/Git/TestGit.java
@@ -0,0 +1,11 @@
+/**
+ * @program: LeetCode
+ * @description: 测试git
+ * @author: tang bd
+ * @create: 2021-12-22 11:53
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
